48d8928c5609eae60d57a90d0e97455fe8093561
howana28 <90485026+howana28@users.noreply.github.com>
2025-04-30 20:10:40 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'; // mantém só uma vez
+import Chatbot from './components/Chatbot';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chatbot />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
