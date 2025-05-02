9cf097e61cb938c81f168e812559677d48073073
howana28 <90485026+howana28@users.noreply.github.com>
2025-05-02 20:16:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './index.css';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login'; // mantém só uma vez
+import Login from './components/Login';
 import Chatbot from './components/Chatbot';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chatbot />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
