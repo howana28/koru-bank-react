f9a113b4088c3b2c260480242db96f74fe191f6d
howana28 <90485026+howana28@users.noreply.github.com>
2025-05-02 20:01:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login'; // mantém só uma vez
 import Chatbot from './components/Chatbot';
 
