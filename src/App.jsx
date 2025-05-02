f904730d270421b16a2d6738d90a35ae3f4e182b
howana28 <90485026+howana28@users.noreply.github.com>
2025-05-02 20:38:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import './index.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Chatbot from './components/Chatbot';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/chat" element={<Chatbot />} />
-      </Routes>
-    </HashRouter>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1 style={{ color: 'green' }}>Olá, Koru Bank está funcionando!</h1>
+    </div>
   );
 }
 
