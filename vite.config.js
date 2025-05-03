2adeb7aad9e8a6c9db7a4f57130232a914a9af64
howana28 <90485026+howana28@users.noreply.github.com>
2025-05-02 22:37:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/koru-bank-react/', // <- nome exato do seu repositório
+  base: '/koru-bank-react/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
 });
